f1f8b36b8cbcb0d8272d1943ebda88cc49ef2f72
EricSemantik <eric@semantik.fr>
2022-06-21 09:28:37 +0200

    init eshop-spring-jpa

---
+++ eshop-spring-jpa/src/main/java/spring/formation/config/ApplicationConfig.java
@@ -0,0 +1,5 @@
+package spring.formation.config;
+
+public class ApplicationConfig {
+
+}
---
+++ eshop-spring-jpa/src/test/java/spring/formation/repo/jpa/Bidon.java
@@ -0,0 +1,14 @@
+package spring.formation.repo.jpa;
+
+
+public class Bidon {
+
+	public static void main(String[] args) {
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
+		
+//		context.close();
+	}
+
+}
