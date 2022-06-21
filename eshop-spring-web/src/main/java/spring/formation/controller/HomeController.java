ea706542b59a4fea63b1c0397bebd8abcc731bce
EricSemantik <eric@semantik.fr>
2022-06-21 16:43:36 +0200

    eshop-spring-web

---
+++ eshop-spring-web/src/main/java/spring/formation/controller/HomeController.java
@@ -0,0 +1,13 @@
+package spring.formation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/accueil")
+	public String home() {
+		return "home";
+	}
+}
---
+++ eshop-spring-web/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Accueil</title>
+</head>
+<body>
+	<h1>Bienvenue sur ESHOP !</h1>
+</body>
+</html>
